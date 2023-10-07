b07ce2abfbc8ae23c7778730684530048cef6b65
Aditya Anand <adityaanand@Adityas-MBP.bbrouter>
2023-10-08 04:07:04 +0530

    added counting lines command

--- cmd/countLine.go
+++ cmd/countLine.go
@@ -10,10 +10,10 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// ccwcCmd represents the ccwc command
-var ccwcCmd = &cobra.Command{
-	Use:   "ccwc",
-	Short: "A brief description of your command",
+// countLineCmd represents the countLine command
+var countLineCmd = &cobra.Command{
+	Use:   "countLine",
+	Short: "Counts the number of lines in a file",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
@@ -21,30 +21,41 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("ccwc called")
-		flagValue, _ := cmd.Flags().GetString("s")
-		//fmt.Println(flagValue, " ", name)
+		fmt.Println("countLine called")
+		flagValue, _ := cmd.Flags().GetString("l")
 		if flagValue != "" {
-			fileInfo, err := os.Stat(flagValue)
+			file, err := os.Stat(flagValue)
 			if err != nil {
 				fmt.Println("Error in Getting File, Please check the path")
 			}
-			fmt.Println(fileInfo.Size())
+			reader, _ := os.ReadFile(flagValue)
+			content := string(reader)
+			fmt.Println(content)
+			lineCount := 0
+			fmt.Println(file, " ", flagValue)
+
+			for _, a := range content {
+				if a == '\n' {
+					fmt.Println(a)
+					lineCount++
+				}
+			}
+			fmt.Println(lineCount)
 		}
+
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(ccwcCmd)
+	rootCmd.AddCommand(countLineCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	ccwcCmd.PersistentFlags().String("foo", "", "A help for foo")
-	ccwcCmd.PersistentFlags().String("s", "", "Gives the Number of Bytes in a File")
+	countLineCmd.PersistentFlags().String("l", "", "Counts the Number of Lines in a file")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// ccwcCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// countLineCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
